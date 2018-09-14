72707018dd948c528324ca93841418c1feaee9c8
王俊翰 <775647773@qq.com>
2018-09-14 12:42:13 +0800

    'beautiful'

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Blog
+from .models import Blog, BlogType
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def blog_list(request):
-    blogs = Blog.published.all()
-    return render(request, 'blog/blog_list.html', {'blogs':blogs})
+    blog_types = BlogType.objects.all()
+    object_list = Blog.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
+    return render(request, 'blog/blog_list.html',
+     {'page':page, 'blogs':blogs ,'blog_types':blog_types})
 
 def blog_detail(request, year, month, day, blog_slug):
     blog = get_object_or_404(Blog,
